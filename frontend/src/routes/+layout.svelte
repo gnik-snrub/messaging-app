2812379bb0fb4a2de77804e76642c238627c7e41
Josiah Morris <josiahbmorris@hotmail.com>
2024-01-29 04:44:23 +0900

    Set global html style

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,3 +3,16 @@
 </svelte:head>
 
 <slot />
+
+<style>
+  :global(html, body) {
+    background-color: #030027;
+    color: #FEE;
+    font-family: Arial, Helvetica, sans-serif;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+  }
+</style>
