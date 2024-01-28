5aee18b1eda6e29f2e82409d342a090081e7fbe0
Josiah Morris <josiahbmorris@hotmail.com>
2024-01-29 04:46:51 +0900

    Added simple layout to chat route

--- frontend/src/routes/chat/+page.svelte
+++ frontend/src/routes/chat/+page.svelte
@@ -5,14 +5,29 @@
 </script>
 
 <main>
-  <section>
+  <section id="userList">
     {#each friendList as friend }
       <button>User to make active</button>
     {/each}
   </section>
-  <section>
+  <section id="messages">
     {#each messages as message }
       <p>Message from active friend</p>
     {/each}
   </section>
 </main>
+
+<style>
+  main {
+    height: 100%;
+    width: 80%;
+    border: 1px solid white;
+    display: flex;
+  }
+  #userList {
+    width: 10%;
+  }
+  #messages {
+    width: 90%;
+  }
+</style>
