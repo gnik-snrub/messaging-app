8df50fbdea2aaa4262bed01ab1c97a09f3ae64c6
Josiah Morris <josiahbmorris@hotmail.com>
2024-02-08 04:25:36 +0900

    Wrapped /login route contents in a main tag

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -14,30 +14,32 @@
   }
 </script>
 
-{#if needsToSignUp}
-<h2>Sign up</h2>
+<main>
+  {#if needsToSignUp}
+  <h2>Sign up</h2>
 
-<button on:click={() => {needsToSignUp = !needsToSignUp}}>Log in</button>
-  
-<form on:submit={signup}>
-  <label for="username">Username: </label>
-  <input type="text" name="username" id="username" bind:value={signupUsername}>
-  <label for="password">Password: </label>
-  <input type="text" name="password" id="password" bind:value={signupPassword}>
-  <label for="favoriteColor">Favorite Color: </label>
-  <input type="color" bind:value={favoriteColor}>
-  <button type="submit">Login</button>
-</form>
-{:else}
-<h2>Log In</h2>
+  <button on:click={() => {needsToSignUp = !needsToSignUp}}>Let me log in</button>
+    
+  <form on:submit={signup}>
+    <label for="username">Username: </label>
+    <input type="text" name="username" id="username" bind:value={signupUsername}>
+    <label for="password">Password: </label>
+    <input type="password" name="password" id="password" bind:value={signupPassword}>
+    <label for="favoriteColor">Favorite Color: </label>
+    <input type="color" bind:value={favoriteColor}>
+    <button type="submit">Sign up</button>
+  </form>
+  {:else}
+  <h2>Log In</h2>
 
-<button on:click={() => {needsToSignUp = !needsToSignUp}}>Sign up</button>
+  <button on:click={() => {needsToSignUp = !needsToSignUp}}>I need to sign up</button>
 
-<form on:submit={login}>
-  <label for="username">Username: </label>
-  <input type="text" name="username" id="username" bind:value={loginUsername}>
-  <label for="password">Password: </label>
-  <input type="text" name="password" id="password" bind:value={loginPassword}>
-  <button type="submit">Login</button>
-</form>
-{/if}
+  <form on:submit={login}>
+    <label for="username">Username: </label>
+    <input type="text" name="username" id="username" bind:value={loginUsername}>
+    <label for="password">Password: </label>
+    <input type="password" name="password" id="password" bind:value={loginPassword}>
+    <button type="submit">Login</button>
+  </form>
+  {/if}
+</main>
