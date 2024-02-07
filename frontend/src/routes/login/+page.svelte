2cf32d78ee5e507de103aa7e9d60002d0f341190
Josiah Morris <josiahbmorris@hotmail.com>
2024-02-08 04:01:56 +0900

    Added login form to /login route

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,5 +1,11 @@
-<h2>Login</h2>
 
-  <form>
-    <!--TODO-->
-  </form>
+<h2>Log In</h2>
+
+
+<form on:submit={login}>
+  <label for="username">Username: </label>
+  <input type="text" name="username" id="username" bind:value={loginUsername}>
+  <label for="password">Password: </label>
+  <input type="text" name="password" id="password" bind:value={loginPassword}>
+  <button type="submit">Login</button>
+</form>
