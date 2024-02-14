ec8dc57e0001a749c5eaad779e0e1256fec203e0
Josiah Morris <josiahbmorris@hotmail.com>
2024-02-15 02:45:41 +0900

    Added styling for userBubble class

--- frontend/src/routes/users/+page.svelte
+++ frontend/src/routes/users/+page.svelte
@@ -16,3 +16,16 @@
 
 <!--Search bar here-->
 <!--List of users here-->
+<style>
+  .userBubble {
+    border-radius: 50%;
+    width: 2em;
+    height: 2em;
+    background-color: rgba(255, 255, 255, 0.1);
+    color: inherit;
+    font-size: 1.5em;
+    border: none;
+    border: 1px solid transparent;
+    margin: 0.2em;
+  }
+</style>
