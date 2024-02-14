c5445e22bde426eb6149ac5c86536dde43b7a0ab
Josiah Morris <josiahbmorris@hotmail.com>
2024-02-15 02:45:23 +0900

    Added user search input, and results list to /users route

--- frontend/src/routes/users/+page.svelte
+++ frontend/src/routes/users/+page.svelte
@@ -1,4 +1,18 @@
 <h2>Add a user as a friend</h2>
 
+<input type="text" bind:value={searchRequest} placeholder="User">
+
+<ul>
+  {#each searchResults as user}
+  <ol>
+    <div class="userBubble" style:background-color={user.favoriteColor}>
+      {user.name.slice(0, 1).toUpperCase()}
+    </div>
+    <span>{user.name}</span>
+    <button on:click={addFriend}>Add friend</button>
+  </ol>
+  {/each}
+</ul>
+
 <!--Search bar here-->
 <!--List of users here-->
