ef511724215382489ba92417277f2a49c175f93b
Josiah Morris <josiahbmorris@hotmail.com>
2024-02-25 03:50:13 +0900

    Styled /users route

--- frontend/src/routes/users/+page.svelte
+++ frontend/src/routes/users/+page.svelte
@@ -48,15 +48,67 @@
 </main>
 
 <style>
+  main {
+    display: flex;
+    flex-direction: column;
+    width: 40%;
+    align-items: center;
+    padding-top: 1.7em;
+    overflow: auto;
+  }
+  a {
+    text-decoration: none;
+    background-color: var(--secondaryColor);
+    color: var(--primaryColor);
+    padding: 0.3em 0.7em;
+    border: 2px solid var(--primaryColor);
+    border-radius: 50vw;
+    transition: 300ms;
+    outline: none;
+  }
+  a:hover,
+  a:focus{
+    --primaryColor: #030027;
+    --secondaryColor: #F2F3D9;
+  }
+  input {
+    background-color: rgba(255, 255, 255, 0.1);
+    color: inherit;
+    font-size: 1em;
+    border-radius: 1em;
+    padding: 0.3em 1em;
+    border: 0;
+    width: 20ch;
+  }
+  ul {
+    overflow: auto;
+    padding: 1em 0;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  ol {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    width: 80%;
+    padding: 0.3em 1.4em;
+  }
+  ol:nth-child(odd) {
+    background-color: rgba(255, 255, 255, 0.02);
+  }
   .userBubble {
     border-radius: 50%;
     width: 2em;
     height: 2em;
-    background-color: rgba(255, 255, 255, 0.1);
     color: inherit;
     font-size: 1.5em;
-    border: none;
     border: 1px solid transparent;
     margin: 0.2em;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 </style>
