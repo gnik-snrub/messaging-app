727ba9d4b1bc5838b67f4f99ec644d5b74c9218f
Josiah Morris <josiahbmorris@hotmail.com>
2024-03-22 04:26:38 +0900

    Added controls to API endpoints

---
+++ backend/controllers/apiController.js
@@ -0,0 +1,24 @@
+
+exports.retrieveMessages = (req, res, next) => {
+  res.next()
+}
+
+exports.sendMessage = (req, res, next) => {
+  res.next()
+}
+
+exports.searchUsers = (req, res, next) => {
+  res.next()
+}
+
+exports.addFriend = (req, res, next) => {
+  res.next()
+}
+
+exports.signup = (req, res, next) => {
+  res.next()
+}
+
+exports.login = (req, res, next) => {
+  res.next()
+}
