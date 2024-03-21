3d57cdc188474a0c61060bd3a9633644948d38a9
Josiah Morris <josiahbmorris@hotmail.com>
2024-03-22 04:26:12 +0900

    Added API endpoints

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,6 +1,15 @@
+const apiController = require('../controllers/apiController')
+
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res, next) => {console.log('you reached the api!'); res.json({api: true})})
+router.get('/retrieveMessages', apiController.retrieveMessages)
+router.post('/send/:messageID', apiController.sendMessage)
+
+router.post('/searchUsers/:searchQuery', apiController.searchUsers)
+router.post('/addFriend', apiController.addFriend)
+
+router.post('/signup', apiController.signup)
+router.post('/login', apiController.login)
 
 module.exports = router
