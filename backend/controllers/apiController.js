e045ffe6ccad9e7fdb8bfadcb5c85473f65925ba
Josiah Morris <josiahbmorris@hotmail.com>
2024-03-25 05:04:50 +0900

    Built backend signup function

--- backend/controllers/apiController.js
+++ backend/controllers/apiController.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
 
 exports.retrieveMessages = (req, res, next) => {
   res.next()
@@ -15,8 +17,35 @@ exports.addFriend = (req, res, next) => {
   res.next()
 }
 
-exports.signup = (req, res, next) => {
-  res.next()
+exports.signup = async (req, res, next) => {
+  const errors = []
+
+  const { username, password, favoriteColor } = req.body
+  const foundUser = await User.findOne({ username: username })
+
+  if (foundUser) {
+    errors.push('Username already exists')
+  }
+
+  if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password) === false) {
+    errors.push('Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character')
+  }
+
+  if (errors.length === 0) {
+    const encryptedPassword = bcrypt.hashSync(password, 10)
+    const newUser = new User({
+      username,
+      password: encryptedPassword,
+      favoriteColor: favoriteColor
+    })
+    await newUser.save()
+
+    console.log(`User ${username} created!`)
+    res.json({ id: newUser._id, errors: errors })
+  } else {
+    console.log(`Errors: ${errors}`)
+    res.json({id: null, errors: errors })
+  }
 }
 
 exports.login = (req, res, next) => {
